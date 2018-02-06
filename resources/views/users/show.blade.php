2ba5904bad185fd87e0b6e7c0a0e0bda4e99ed0c
Unknown <jplatzbecker99@gmail.com>
2018-02-06 11:35:03 +0100

    Show user

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Users</div>
+
+                    <div class="panel-body">
+                        @foreach ($users as $user)
+                            <article>
+                                <h4>
+                                    {{ $user->title }}
+                                </h4>
+                            </article>
+                            <div class="body">{{ $thread->body }}</div>
+                            <hr>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
