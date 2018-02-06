c2a1d38789e337c9ba9c58fe06e797d2dd011ba9
Unknown <jplatzbecker99@gmail.com>
2018-02-06 14:18:27 +0100

    User profile id added

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,6 +1,6 @@
 <div class="panel panel-default">
     <div class="panel-heading">
-        <a href="#">
+        <a href="/users/{{ $reply->owner->id }}">
             {{ $reply->owner->name }}
         </a>    said {{ $reply->created_at->diffForHumans() }}...
     </div>
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <a href="#">{{ $thread->creator->name }}</a> posted:
+                        <a href="/users/{{ $thread->creator->id }}">{{ $thread->creator->name }}</a> posted:
                         {{ $thread->title }}
                     </div>
 
