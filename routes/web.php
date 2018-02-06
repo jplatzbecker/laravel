858e0a6f96da22fde264cacdd3de8cdfa5cb0d61
Unknown <jplatzbecker99@gmail.com>
2018-02-06 14:18:42 +0100

    Profile page added

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/threads', 'ThreadsController@index');
 Route::get('/users', 'UsersController@index');
+Route::get('/users/{user}', 'UsersController@show');
 Route::post('/threads', 'ThreadsController@store');
 Route::get('/threads/{thread}', 'ThreadsController@show');
 Route::post('/threads/{thread}/replies', 'RepliesController@store');
